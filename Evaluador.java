64971673adc50fcc9f3adf240b1e3f97db0bf6c2
genchile56 genchile <catalinafuenzalidam98@gmail.com>
2024-08-28 12:24:57 -0400

    evaluador pt1 real, con modificaciones

--- Evaluador.java
+++ Evaluador.java
@@ -6,63 +6,80 @@ public class Evaluador {
 
     public static void main(String[] args) {
         Scanner miScanner = new Scanner(System.in);
-        Scanner miScanner2 = new Scanner(System.in);
-        Scanner miScanner3 = new Scanner(System.in);
 
-        System.out.println("Ingresa tu nota");
-
+        System.out.println("Quimica");
+        System.out.println("Ingresa tu nota: ");
         int notas = miScanner.nextInt();
-        while (notas >= 10 || notas <= 1) {
+
+
+        while (notas > 10 || notas < 1) {
             System.out.println("La nota es invalida");
+            System.out.println("ingresa tu nota: ");
+            notas = miScanner.nextInt();
+        }
+        int quimica = notas;
+
+        System.out.println("ingles");
+        System.out.println("ingresa tu nota:");
+        notas = miScanner.nextInt();
+
+        while (notas > 10 || notas < 1) {
+            System.out.println("La nota es invalida");
+            System.out.println("ingresa tu nota: ");
+            notas = miScanner.nextInt();
+        }
+
+        int ingles = notas;
+
+        System.out.println("matematica");
+        System.out.println("ingresa tu nota:");
+        notas = miScanner.nextInt();
+
+        while (notas > 10 || notas < 1) {
+            System.out.println("La nota es invalida");
+            System.out.println("ingresa tu nota: ");
+            notas = miScanner.nextInt();
+        }
+        int matematica = notas;
 
 
 
-            System.out.println("Bienvenido a tu evaluador de notas, ingresa tu calificación de Quimica: ");
-            int calificacionQuimica = miScanner.nextInt();
 
-            System.out.println("por favor ingresa tu calificación de Ingles:");
-            int calificacionIngles = miScanner2.nextInt();
+        //QUIMICA//
 
-            System.out.println("ahora tu calificacion de matematicas.");
-            int calificacionMatematicas = miScanner3.nextInt();
+         if (quimica == 10) {
+            System.out.println("Felicidades, aprobaste la materia");
+         } else if (quimica > 5 && quimica <= 8) {
+            System.out.println("Tienes una califición aceptable");
+         } else if (quimica > 3 && quimica <= 5) {
+            System.out.println("por poco, esta calificación es insuficiente");
+         } else {
+            System.out.println("No Aprobado");
+         }
 
+         //INGLEES//
 
-            //QUIMICA//
+         if (ingles == 10) {
+            System.out.println("Felicidades, aprobaste la materia");
+         } else if (ingles > 5 && ingles <= 8) {
+            System.out.println("Tienes una calificación aceptable");
+         } else if (ingles > 3 && ingles <= 5) {
+            System.out.println("por poco, esta calificación es insuficiente");
+         } else {
+             System.out.println("No Aprobado");
+         }
 
-            if (calificacionQuimica == 10) {
-                System.out.println("Felicidades, aprobaste la materia");
-            } else if (calificacionQuimica > 5 && calificacionQuimica <= 8) {
-                System.out.println("Tienes una califición aceptable");
-            } else if (calificacionQuimica > 3 && calificacionQuimica <= 5) {
-                System.out.println("por poco, esta calificación es insuficiente");
-            } else {
-                System.out.println("No Aprobado");
-            }
+            //MATEMATICA/
 
-            //INGLEES//
-
-            if (calificacionIngles == 10) {
-                System.out.println("Felicidades, aprobaste la materia");
-            } else if (calificacionIngles > 5 && calificacionIngles <= 8) {
-                System.out.println("Tienes una calificación aceptable");
-            } else if (calificacionIngles > 3 && calificacionIngles <= 5) {
-                System.out.println("por poco, esta calificación es insuficiente");
-            } else {
-                System.out.println("No Aprobado");
-
-                //MATEMATICA/
-            }
-
-            if (calificacionMatematicas == 10) {
-                System.out.println("Felicidades, aprobaste la materia");
-            } else if (calificacionMatematicas > 5 && calificacionQuimica <= 8) {
-                System.out.println("Tienes una calificación aceptable");
-            } else if (calificacionMatematicas > 3 && calificacionQuimica <= 5) {
-                System.out.println("por poco, esta calificación es insuficiente");
-            } else {
-                System.out.println("No Aprobado");
-            }
-
+         if (matematica == 10) {
+            System.out.println("Felicidades, aprobaste la materia");
+         } else if (matematica > 5 && matematica <= 8) {
+            System.out.println("Tienes una calificación aceptable");
+         } else if (matematica > 3 && matematica <= 5) {
+            System.out.println("por poco, esta calificación es insuficiente");
+         } else {
+             System.out.println("No Aprobado" );
+         }
         }
     }
-}
+
